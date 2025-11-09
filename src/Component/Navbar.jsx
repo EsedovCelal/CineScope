35b2c0eabdde1e6837f3cdd278c7d1a95cd70c47
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2025-11-09 12:18:12 +0400

    add primereact and primeicons

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,41 +1,39 @@
 import temp_logo from "../images/temp_logo.jpg";
+import { Dropdown } from "primereact/dropdown";
+import { useState } from "react";
 const Navbar = () => {
-  const options = ["Games", "Movies"];
+  const options = ["All", "Games", "Movies", "Tv series"];
+  const [selectedItem, setSelectedItem] = useState(null);
   return (
     <div className="bg-[black] w-300 text-[white] h-20 flex items-center">
-      <img className="h-10 w-10" src={temp_logo} />
-      <p>Salam</p>
+      <img className="h-7 w-20 rounded-lg" src={temp_logo} />
       <div className="flex justify-between items-center relative ">
-        <div className="absolute">
-          <button
-            className="btn"
-            popoverTarget="popover-1"
-            style={{ anchorName: "--anchor-1" }}
-          >
-            All
-          </button>
-          <ul
-            className="dropdown menu w-52 rounded-box bg-base-100 shadow-sm"
-            popover="auto"
-            id="popover-1"
-            style={{ positionAnchor: "--anchor-1" }}
-          >
-            <li>
-              <a>All</a>
-            </li>
-            <li>
-              <a>Games</a>
-            </li>
-            <li>
-              <a>Movies</a>
-            </li>
-          </ul>
+        <div className="rounded-l-lg bg-[white] h-7">
+          <Dropdown
+            value={selectedItem}
+            onChange={(e) => setSelectedItem(e.value)}
+            options={options}
+            placeholder={options[0]}
+            className="w-full"
+            pt={{
+              root: { className: "h-7" },
+              list: { className: "w-50 h-full p-1" },
+              input: { className: "p-1 border-none text-[black]" },
+            }}
+          />
         </div>
         <div>
-          <input type="text" className="w-100 h-10 bg-[white] rounded-box" />
+          <input
+            type="text"
+            className="w-100 h-7 bg-[white] text-[black] rounded-r-lg p-1"
+          />
         </div>
-        <div className="absolute">
-          <svg viewBox="0 0 24 24" fill="currentColor" class="size-6">
+        <div className="absolute right-2">
+          <svg
+            viewBox="0 0 24 24"
+            fill="grey"
+            className="size-5 hover:fill-[black] cursor-pointer"
+          >
             <path
               fill-rule="evenodd"
               d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import "primereact/resources/themes/lara-light-blue/theme.css"; // Theme
+import "primereact/resources/primereact.min.css"; // Core CSS
+import "primeicons/primeicons.css"; // Icons
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
