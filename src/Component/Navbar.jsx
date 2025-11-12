b0a418f3bd01ce7f0b9f1f18733c27d32858815d
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2025-11-12 21:07:35 +0400

    modify navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,8 +1,6 @@
-import temp_logo from "../images/temp_logo.jpg";
-import Menu from "./Menu";
-import { Dropdown } from "primereact/dropdown";
 import { useState, useEffect } from "react";
 import { InputText } from "primereact/inputtext";
+import { Link } from "@mui/material";
 
 const Navbar = () => {
   const options = ["All", "Games", "Movies", "Tv series"];
@@ -11,6 +9,7 @@ const Navbar = () => {
   const [externalData, setExternalData] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [isFocused, setIsFocused] = useState(false);
   const handleSearch = async () => {
     try {
       const res = await fetch(
@@ -25,46 +24,33 @@ const Navbar = () => {
   };
   console.log(externalData.results);
   return (
-    <div className="bg-[black] w-300 text-[white] h-20 flex items-center">
-      <img className="h-7 w-20 rounded-lg" src={temp_logo} />
-      <Menu />
-      <div className="flex justify-between items-center relative ">
-        <div className="absolute rounded-lg bg-[white] h-7">
-          <Dropdown
-            value={dropItem}
-            onChange={(e) => setDropItem(e.value)}
-            options={options}
-            placeholder={options[0]}
-            className="w-full"
-            pt={{
-              root: { className: "h-7" },
-              list: { className: "w-50 h-full p-1" },
-              input: { className: "p-1 text-[black] border-0" },
-              trigger: { className: "size-3 pt-3.5" },
-            }}
+    <div className=" w-330 text-[white] h-20 flex items-center bg-[grey] rounded-lg px-2">
+      <p className="text-2xl">myv.</p>
+      <div className="relative flex items-center justify-end w-full">
+        <svg viewBox="0 0 24 24" fill="grey" className="size-4 left-6 relative">
+          <path
+            fillRule="evenodd"
+            d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+            clipRule="evenodd"
           />
-        </div>
-        <div>
-          <InputText
-            type="text"
-            value={inputItem || ""}
-            onChange={(e) => setInputItem(e.target.value)}
-            className="w-100 h-7 bg-[white] text-[black] rounded-lg pl-11"
-          />
-        </div>
-        <div className="absolute right-2">
-          <svg
-            viewBox="0 0 24 24"
-            fill="grey"
-            className="size-5 hover:fill-[black] cursor-pointer"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <button onClick={handleSearch}>find</button>
+        </svg>
+        <input
+          type="text"
+          value={inputItem || ""}
+          onChange={(e) => setInputItem(e.target.value)}
+          className={`${
+            isFocused ? "w-full" : "w-64"
+          } border-gray-300 transition-all duration-300 ease-in-out h-9 bg-[white] text-[black] rounded-lg pl-7 outline-0`}
+          placeholder="Search you game"
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
+        <button className="pl-5" onClick={handleSearch}>
+          find
+        </button>
+        <div className="w-50">
+          <Link className="pl-5">Log in</Link>
+          <Link className="pl-5">Sign Up</Link>
         </div>
       </div>
     </div>
