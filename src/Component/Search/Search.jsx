444b199de17f934c20c70f5e89642b79edff0eeb
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2025-12-25 16:42:57 +0400

    Update Search.jsx

--- src/Component/Search/Search.jsx
+++ src/Component/Search/Search.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { Link } from "@mui/material";
 
 const Search = () => {
-  const [selectedItem, setSelectedItem] = useState("movie");
+  const [selectedItem, setSelectedItem] = useState("");
   const [inputItem, setInputItem] = useState("");
   const [result, setResult] = useState([]);
   const [error, setError] = useState(null);
@@ -12,15 +12,15 @@ const Search = () => {
   const items = [
     {
       id: 1,
-      title: "Movie",
+      title: "movie",
     },
     {
       id: 2,
-      title: "Tv Shows",
+      title: "tv",
     },
     {
       id: 3,
-      title: "People",
+      title: "people",
     },
     {
       id: 4,
@@ -38,8 +38,8 @@ const Search = () => {
     { id: 8, title: "Awards" },
   ];
 
-  const handleSelect = (id) => {
-    setSelectedItem(id);
+  const handleSelect = (title) => {
+    setSelectedItem(title);
   };
   useEffect(() => {
     if (!inputItem) return;
@@ -63,27 +63,44 @@ const Search = () => {
       console.error("Error:", error);
     }
   };
+  const handleChange = (e) => {
+    const newValue = e.target.value;
+    setInputItem(newValue);
+    localStorage.setItem("searchInput", newValue);
+  };
+
+  useEffect(() => {
+    const savedValue = localStorage.getItem("searchInput");
+    if (savedValue !== null) {
+      setInputItem(savedValue);
+    }
+  }, []);
+
   return (
-    <div className="">
-      <div>
-        <input
-          className="w-full"
-          type="text"
-          onChange={(e) => setInputItem(e.target.value)}
-        />
-      </div>
-      <div className="flex justify-center">
-        <div className="w-[70%] gap-10 flex justify-between">
-          <div className="w-50 ">
-            <div className="">
-              {items.map((item) => {
-                const isSelected = selectedItem === item.id;
-                return (
-                  <div
-                    key={item.id}
-                    onClick={() => handleSelect(item.id)}
-                    className={`pl-5
-                  bg-white rounded-lg shadow-md cursor-pointer
+    <div className="flex justify-center">
+      <div className="w-[70%] ">
+        <div>
+          <input
+            value={inputItem}
+            className="w-full border-[#01B4E4] border-2 rounded-lg p-3 mb-5 mt-5"
+            type="text"
+            onChange={handleChange}
+            placeholder="Search some"
+          />
+        </div>
+        <div className=" gap-10 flex justify-between">
+          <div className="w-80 rounded-lg">
+            <div className="flex justify-center items-center bg-[#01B4E4] h-20 font-bold ">
+              <p>Search results</p>
+            </div>
+            {items.map((item) => {
+              const isSelected = selectedItem === item.title;
+              return (
+                <div
+                  key={item.id}
+                  onClick={() => handleSelect(item.title)}
+                  className={`pl-5
+                  bg-white shadow-md cursor-pointer
                   transition-all duration-200 border-2
                   ${
                     isSelected
@@ -91,23 +108,25 @@ const Search = () => {
                       : "border-transparent hover:border-indigo-200 hover:shadow-lg"
                   }
                 `}
+                >
+                  <h3
+                    className={`text-lg font-semibold mb-2 ${
+                      isSelected ? "text-indigo-700" : "text-gray-800"
+                    }`}
                   >
-                    <h3
-                      className={`text-lg font-semibold mb-2 ${
-                        isSelected ? "text-indigo-700" : "text-gray-800"
-                      }`}
-                    >
-                      {item.title}
-                    </h3>
-                  </div>
-                );
-              })}
-            </div>
+                    {item.title}
+                  </h3>
+                </div>
+              );
+            })}
           </div>
           <div className="w-full">
             {result &&
               result.map((item, index) => (
-                <div className="border flex mb-5">
+                <div
+                  className="border flex mb-5 key={index} rounded-lg shadow-md gap-5"
+                  key={index}
+                >
                   <img
                     className="rounded-lg shadow-lg"
                     src={`https://image.tmdb.org/t/p/w92${item.poster_path}`}
